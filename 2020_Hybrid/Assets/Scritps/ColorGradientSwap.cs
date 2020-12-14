824e19750b3ff9a4c7525df3b95247e69a96144b
BlackLotusM <35292137+BlackLotusM@users.noreply.github.com>
2020-12-14 15:26:14 +0100

    ProjectPush

---
+++ 2020_Hybrid/Assets/Scritps/ColorGradientSwap.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorGradientSwap : MonoBehaviour
+{
+    public Gradient myGradient;
+    public float strobeDuration = 2f;
+
+    public void Update()
+    {
+        float t = Mathf.PingPong(Time.time / strobeDuration, 1f);
+        Camera.main.backgroundColor = myGradient.Evaluate(t);
+    }
+}
---
+++ 2020_Hybrid/Assets/Scritps/scrpt_ButtonHandler.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using Mirror;
+using UnityEngine.SceneManagement;
+
+public class scrpt_ButtonHandler : NetworkBehaviour, IPointerEnterHandler, IPointerDownHandler, IPointerExitHandler
+{
+    [SerializeField]
+    private bool Option = false;
+    [SerializeField]
+    private bool Host = false;
+    [SerializeField]
+    private bool Join = false;
+    [SerializeField]
+    private bool Exit = false;
+    [SerializeField]
+    private bool IsStop = false;
+    [SerializeField]
+    private GameObject OptionPanel = null;
+
+    [SerializeField]
+    private GameObject HostPanel = null;
+
+    [SerializeField]
+    private GameObject JoinPanel = null;
+    [SerializeField]
+    private GameObject MainPanel = null;
+
+    [SerializeField]
+    private Animator anim = null;
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        anim.SetBool("Pressed", true);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        anim.SetBool("Hover", true);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        anim.SetBool("Hover", false);
+        anim.SetBool("Pressed", false);
+    }
+
+    public void EnablePanel()
+    {
+        if (IsStop)
+        {
+            NetworkManager nm = FindObjectOfType<NetworkManager>();
+            if (isServer)
+            {
+                nm.StopHost();
+            }
+            else { nm.StopClient(); }
+            SceneManager.LoadScene("Main");
+        }
+        if (Option)
+        {
+            EnableOption();
+        }
+        if (Host)
+        {
+            EnableHost();
+        }
+        if (Join)
+        {
+            EnableJoin();
+        }
+        if (Exit)
+        {
+            Application.Quit();
+        }
+        MainPanel.SetActive(false);
+    }
+    private void EnableOption()
+    {
+        OptionPanel.SetActive(true);
+        HostPanel.SetActive(false);
+        JoinPanel.SetActive(false);
+    }
+
+    private void EnableHost()
+    {
+        OptionPanel.SetActive(false);
+        HostPanel.SetActive(true);
+        JoinPanel.SetActive(false);
+    }
+
+    private void EnableJoin()
+    {
+        OptionPanel.SetActive(false);
+        HostPanel.SetActive(false);
+        JoinPanel.SetActive(true);
+    }
+}
